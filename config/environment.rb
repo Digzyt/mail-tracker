def0994139661b0f2001a8688f8e6f29159601f3
Digzyt <donyegon@gmail.com>
2015-11-02 17:59:46 +0000

    Mailer modification

--- config/environment.rb
+++ config/environment.rb
@@ -8,12 +8,14 @@ ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net',
   :port => '587',
   :authentication => :plain,
-  :user_name => ENV['app43203634@heroku.com'],
-  :password => ENV['rn8looq03317'],
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'heroku.com',
   :enable_startstls_auto => true
 }
 =begin
+:user_name => ENV['app43203634@heroku.com'],
+  :password => ENV['rn8looq03317'],
 Pony.options = {
       :via => :smtp,
       :via_options => {
