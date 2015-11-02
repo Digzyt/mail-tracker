c337c67ac823344246b2c4da773a136ae51b4daa
Digzyt <donyegon@gmail.com>
2015-11-02 16:29:14 +0000

    Added sendgrid credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
-  config.web_console.whitelisted_ips = '197.237.122.234'
 end
